b4bcc8e04bcc75b62b4139b85ba7283299a9895c
iamshubhankarkhare <iamshubhankarkhare@gmail.com>
2021-06-13 00:10:39 +0530

    add connected scatter chart

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import * as d3 from 'd3';
 import './App.css';
 import BarChart from './charts/barChart';
+import ConnectedScatter from './charts/connectedScatter';
 import DoughnutChart from './charts/doughnutChart';
 
 const datas = [
@@ -10,6 +11,65 @@ const datas = [
   [25, 18, 27],
 ];
 var i = 0;
+
+const data2 = [
+  {
+    date: '1-May-12',
+    close: 58.13,
+  },
+  {
+    date: '30-Apr-12',
+    close: 53.98,
+  },
+  {
+    date: '27-Apr-12',
+    close: 67,
+  },
+  {
+    date: '26-Apr-12',
+    close: 89.7,
+  },
+  {
+    date: '25-Apr-12',
+    close: 99,
+  },
+  {
+    date: '18-Apr-12',
+    close: 443.34,
+  },
+  {
+    date: '17-Apr-12',
+    close: 543.7,
+  },
+  {
+    date: '16-Apr-12',
+    close: 580.13,
+  },
+  {
+    date: '13-Apr-12',
+    close: 605.23,
+  },
+  {
+    date: '30-Mar-12',
+    close: 599.55,
+  },
+  {
+    date: '29-Mar-12',
+    close: 609.86,
+  },
+  {
+    date: '28-Mar-12',
+    close: 617.62,
+  },
+  {
+    date: '27-Mar-12',
+    close: 614.48,
+  },
+  {
+    date: '26-Mar-12',
+    close: 606.98,
+  },
+];
 
 function App() {
   const [data, setData] = useState([]);
@@ -25,6 +85,7 @@ function App() {
         innerRadius={80}
         outerRadius={100}
       />
+      <ConnectedScatter data={data2} />
     </div>
   );
 }
---
+++ src/charts/connectedScatter.js
@@ -0,0 +1,118 @@
+import * as d3 from 'd3';
+import React, { useRef, useEffect } from 'react';
+
+function ConnectedScatter({ width, height, data }) {
+  console.log(data);
+  const svgRef = React.useRef();
+  useEffect(() => {
+    const margin = { top: 20, right: 20, bottom: 30, left: 50 },
+      width = 560 - margin.left - margin.right,
+      height = 200 - margin.top - margin.bottom;
+
+    // parse the date / time
+    const parseTime = d3.timeParse('%d-%b-%y');
+
+    // set the ranges
+    const x = d3.scaleTime().range([0, width]);
+    const y = d3.scaleLinear().range([height, 0]);
+
+    // define the line
+    const valueline = d3
+      .line()
+      .x(function (d) {
+        return x(d.date);
+      })
+      .y(function (d) {
+        return y(d.close);
+      });
+
+    // append the svg obgect to the body of the page
+    // appends a 'group' element to 'svg'
+    // moves the 'group' element to the top left margin
+    const svg = d3.select(svgRef.current);
+    svg
+      .attr('width', width + margin.left + margin.right)
+      .attr('height', height + margin.top + margin.bottom)
+      .append('g')
+      .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
+
+    // format the data
+    data.forEach(function (d) {
+      d.date = parseTime(d.date);
+      d.close = +d.close;
+      console.log(d.date);
+    });
+
+    // Scale the range of the data
+    x.domain(
+      d3.extent(data, function (d) {
+        return d.date;
+      })
+    );
+    y.domain([
+      0,
+      d3.max(data, function (d) {
+        return d.close;
+      }),
+    ]);
+
+    // Add the valueline path.
+    // svg.append('path').data([data]).attr('class', 'line').attr('d', valueline);
+
+    svg
+      .append('path')
+      .datum(data)
+      .attr('fill', 'none')
+      .attr('stroke', '#69b3a2')
+      .attr('stroke-width', 1.5)
+      .attr(
+        'd',
+        d3
+          .line()
+          .x(function (d) {
+            return x(d.date);
+          })
+          .y(function (d) {
+            return y(d.close);
+          })
+      );
+    // Add the scatterplot
+    svg
+      .selectAll('dot')
+      .data(data)
+      .enter()
+      .append('circle')
+      .attr('r', 5)
+      .attr('cx', function (d) {
+        return x(d.date);
+      })
+      .attr('cy', function (d) {
+        return y(d.close);
+      })
+      .attr('fill', '#69b3a2');
+
+    var xAxis = d3.axisBottom(x).ticks(7).tickFormat(d3.timeFormat('%a'));
+    var yAxis = d3.axisLeft(y).ticks(7);
+
+    // Add the X Axis
+    svg
+      .append('g')
+      .attr('transform', `translate(${margin.right * 2},${height})`)
+      .call(xAxis);
+
+    // Add the Y Axis
+    svg
+      .append('g')
+      .attr('transform', `translate(${margin.right * 2},0)`)
+      .call(yAxis);
+  }, []);
+
+  return (
+    <>
+      <svg ref={svgRef}></svg>
+      <br />
+    </>
+  );
+}
+
+export default ConnectedScatter;
